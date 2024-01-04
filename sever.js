93f323839a20ff0a814f11db53ef0cfcefda1927
Sepidehri <sepidriahi@gmail.com>
2024-01-04 22:27:13 +0100

    functions added in server.js

--- sever.js
+++ sever.js
@@ -20,10 +20,118 @@ app.use(session({
   app.use(express.static(path.join(__dirname, 'files')));
   app.use(bodyParser.json());
   app.use(cookieParser());
-  app.use(express.json())
+  app.use(express.json());
+
+
+  app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/dashboard.html');
+  });
+
+  app.get('/account', (req, res) => {
+    res.sendFile(__dirname + '/account.html');
+  });
+
+  app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/register.html');
+  });
+
+  app.post('/signup', async (req, res) => {
+    try {
+      const user = await User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 8),
+      });
+  
+     res.send({ message: "User registered successfully!" });
+     
+    } catch (error) {
+      res.status(500).send({ message: error.message });
+    }
+  });
+
+  app.post('/login', async (req, res) => {
+    try {
+      const user = await User.findOne({
+        where: {
+          username: req.body.username,
+        },
+      });
+  
+      if (!user) {
+        return res.status(404).send({ message: "User Not found." });
+      }
+  
+      const passwordIsValid = bcrypt.compareSync(
+        req.body.password,
+        user.password
+      );
+  
+      if (!passwordIsValid) {
+        return res.status(401).send({
+          message: "Invalid Password!",
+        });
+      }
+      req.session.username=req.body.username;
+      return res.status(200).send();
+    } catch (error) {
+      return res.status(500).send({ message: error.message });
+    }
+  });
+
+  app.post('/logout', (req, res) => {
+    // Clear the session data
+    req.session.destroy((err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Server Error');
+      }
+      
+      return res.status(200).send('Logout successful');
+    });
+  });
+  
+
+  app.get('/account-detail', async (req, res) => {
+    try {
+      console.log(req.session)
+      var usename = req.session.username ;
+      const user = await User.findOne({
+        where: {
+          username: usename,
+        },
+      });
+  
+      if (!user) {
+        return res.status(401).send({ message: "Unauthorized" });
+      }
+
+  
+      return res.status(200).send({username:user.username, email:user.email});
+    } catch (error) {
+      return res.status(500).send({ message: error.message });
+    }
+  });
+
+   // PUT route to update a user
+   app.put('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id);
+    const { name, email } = req.body;
+  
+    // Find the user with the specified ID
+    const userToUpdate = users.find(user => user.id === userId);
+    if (!userToUpdate) {
+      return res.status(404).send('User not found');
+    }
+  
+    // Update the user properties
+    userToUpdate.name = name || userToUpdate.name;
+    userToUpdate.email = email || userToUpdate.email;
+  
+    res.send('User updated successfully');
+  });
 
   
   app.listen(3000, () => {
     console.log('Server listening on port 3000');
   });
-  
